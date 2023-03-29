52448468965c219c1d36e9e5969aadddd24404dc
seray-ahmed <seray.ahmed1@gmail.com>
2023-03-29 23:48:37 +0300

    Task

---
+++ exercise3/exercise3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of wheels (n): ");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n < 3 || n >= 50)
+            {
+                Console.WriteLine("Invalid input. N must be between 3 and 50.");
+                return;
+            }
+
+            for (int cars = 0; cars <= n / 4; cars++)
+            {
+                for (int trucks = 0; trucks <= n / 6; trucks++)
+                {
+                    int remainingWheels = n - (4 * cars) - (6 * trucks);
+                    if (remainingWheels % 3 == 0)
+                    {
+                        int motorcycles = remainingWheels / 3;
+                        if (motorcycles >= 0)
+                        {
+                            Console.WriteLine($"{cars} {trucks} {motorcycles}");
+                            Console.ReadKey();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
